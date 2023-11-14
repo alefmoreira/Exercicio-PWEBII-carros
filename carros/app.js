e9d42959eb49e5a155c86735b1d4a61444a16189
Alef Moreira <79259745+alefmoreira@users.noreply.github.com>
2023-11-13 23:55:26 -0300

    Add files via upload

---
+++ carros/app.js
@@ -0,0 +1,30 @@
+
+const getMarcas = () => {
+
+    const tarefas = fetch('https://raw.githubusercontent.com/filippofilip95/car-logos-dataset/master/logos/data.json')
+
+
+    tarefas
+        .then(resposta => resposta.json())
+        .then( marcas =>{
+
+            const ul = document.createElement('ul')
+            marcas.forEach(marca => {
+                const li = document.createElement('li')
+                const nome = document.createElement('p')
+                const logo = document.createElement('img')
+                logo.src = marca.image?.optimized
+                nome.textContent = `Nome: ${marca.name}`
+                li.appendChild(nome)
+                li.appendChild(logo)
+                ul.appendChild(li)
+                console.log(marca)
+
+            })
+            document.body.appendChild(ul)
+        })
+        .catch(erro => console.log(erro))
+
+        }
+const btnMarcas = document.querySelector("#marcas")
+btnMarcas.addEventListener("click", getMarcas)
